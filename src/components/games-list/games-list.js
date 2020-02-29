2df5b23e93c987210471e25cdc5080cf9c918077
Francisco Santos <fwfs1985@gmail.com>
2020-02-29 03:09:26 +0000

    wip game list

---
+++ src/components/games-list/games-list.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Link from 'next/link'
+import styled from 'styled-components'
+import { Row, Col } from 'react-awesome-styled-grid'
+
+const GameCard = styled(() => (
+  <Col xs={4} as='li'>
+    <Link href={`/${game.slug}`}>
+      <a>
+        {game.cover && game.cover.url && <img src={game.cover.url} alt="" />}
+        <label>{game.name}</label>
+      </a>
+    </Link>
+  </Col>
+))`
+
+`
+
+const GamesList = styled(({ className, games }) => {
+  return (
+    <div className={className}>
+      <Row as='ul'>
+        {games.map((game) => (<GameCard key={game.id} game={game} />))}
+      </Row>
+    </div>
+  )
+})`
+  ul {
+    list-style-type: none;
+  }
+`
+
+export default GamesList
