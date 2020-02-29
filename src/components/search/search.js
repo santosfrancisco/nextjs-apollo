145f1f12326fcf51bf16a208c173aec6fc68b87e
Francisco Santos <fwfs1985@gmail.com>
2020-02-29 03:08:48 +0000

    search input

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import styled from 'styled-components'
+import { MdSearch } from 'react-icons/md'
 
 const Search = styled(({ className, onSubmitSearch, value }) => {
   const inputRef = useRef()
@@ -11,14 +12,30 @@ const Search = styled(({ className, onSubmitSearch, value }) => {
   return (
     <div className={className}>
       <input
+        className="search-input"
         ref={inputRef}
         type="text"
         onKeyDown={event => event.key === 'Enter' && handleSubmitSearch()}
         defaultValue={value}
       />
+      <MdSearch
+        className="search-button"
+        size={24}
+        onClick={handleSubmitSearch}
+      />
     </div>
   )
 })`
-
+display: flex;
+.search-input {
+  border: 1px solid #000;
+  border-radius: 3px 0 0 3px;
+}
+.search-button {
+  cursor: pointer;
+  border: 1px solid;
+  border-left: none;
+  border-radius: 0 3px 3px 0;
+}
 `
 export default Search
